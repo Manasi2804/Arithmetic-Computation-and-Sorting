3680578dae34329073856585285c1779b284632d
Manasi2804 <shahmanasi281996@gmail.com>
2020-03-28 11:18:02 +0530

    Added: Sort the elements of array in descending order

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -11,9 +11,25 @@ computationAnsDictionary[key++]=$(( $firstnumber+$secondnumber/$thirdnumber ))
 computationAnsDictionary[key++]=$(( $firstnumber%$secondnumber+$thirdnumber ))
 
 printf "Values of dictionary:" ${computationAnsDictionary[@]}
+
 counter=0
 for value in ${computationAnsDictionary[@]}
 do
 	computationAnsArray[$(( counter++ ))]=$value
 done
 printf "Values of array: " ${computationAnsArray[@]}
+
+for ((start=0; start<key; start++))
+do
+	for((row=$(($start+1 )); row<key; row++))
+	do
+		if [[ computationAnsArray[start] -lt  computationAnsArray[row] ]]
+		then
+			tempVariable=${computationAnsArray[start]}
+			computationAnsArray[start]=${computationAnsArray[row]}
+			computationAnsArray[row]=$tempVariable
+		fi
+	done
+done
+printf "Array element is desending order is:"${computationAnsArray[@]}
+

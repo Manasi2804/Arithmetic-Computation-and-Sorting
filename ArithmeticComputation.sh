a299aa3329fd3152b14d48b738cd5508fdaeaabf
Manasi2804 <shahmanasi281996@gmail.com>
2020-03-28 10:32:36 +0530

    Added: Compute the value of (a+b*c)

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -x
 
 printf "Enter three numbers to perform arithmetic computation:"
-read firstnumber secondnumber thirdnumber
-
+read firstnumber
+read secondnumber
+read thirdnumber
+key=1
+computationAnsDictionary[key++]=$(( $firstnumber+$secondnumber*$thirdnumber ))
